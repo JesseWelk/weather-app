af5731ce1a1e2db845ea72c09f780720a75e94ec
JesseWelk <jwelk@my.bcit.ca>
2024-02-13 00:32:21 -0800

    fixed app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,204 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
+import { HourWeatherResponse } from "./WeatherContext";
+import { ForecastWeatherResponse } from "./WeatherContext";
 
 export default function Home() {
+
+  const [showCityName, setShowCityName] = useState(false);
+  const [showExtra, setShowExtra] = useState(false);
+  const [city, setCity] = useState("");
+
+  function responseLog() {
+    fetchHourWeather().then((response) => {
+      setHourWeather(response)
+      setShowCityName(true);
+      setShowExtra(true);
+    })
+    fetchWeatherForecast().then((response) => {
+      setWeatherForecast(response)
+    })
+  }
+
+  const [hourWeather, setHourWeather] = useState<HourWeatherResponse>();
+  const [weatherForecast, setWeatherForecast] = useState<ForecastWeatherResponse>();
+
+  async function fetchHourWeather(): Promise<HourWeatherResponse> {
+    const apiKey = '01e286d64cc5dab9d9135b756d38e78f';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const data: HourWeatherResponse = await response.json();
+    return data;
+  }
+
+  async function fetchWeatherForecast(): Promise<ForecastWeatherResponse> {
+    const apiKey = '01e286d64cc5dab9d9135b756d38e78f';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
+    const data: ForecastWeatherResponse = await response.json();
+    return data;
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <main style={{
+        overflowX: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        fontFamily: "sans-serif",
+      }}>
+
+        <header style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "100%",
+          padding: "20px",
+          justifyContent: "space-evenly",
+          backgroundColor: "#3b3b3b",
+          borderRadius: "0px 0px 30px 30px"
+        }}>
+
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", paddingBottom: "20px" }}>
+            <img src="/images/logo.png" style={{ width: "250px", }} alt="SkyTrackers" />
+          </div>
+          <div style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            textAlign: "center",
+          }}>
+
+            <input placeholder="Search Weather" type="text" value={city} onChange={e => setCity(e.target.value)} style={{
+              width: "200px",
+              height: "15px",
+              minHeight: "15px",
+              minWidth: 125,
+              padding: "10px",
+              fontSize: "16px",
+              borderRadius: "20px 0px 0px 20px",
+              borderWidth: "4px",
+              borderColor: "lightblue",
+            }}>
+            </input>
+
+            <button onClick={responseLog} style={{
+              width: "100px",
+              height: "60px",
+              minHeight: 20,
+              minWidth: 60,
+              padding: "10px",
+              fontSize: "18px",
+              backgroundColor: "lightblue",
+              borderRadius: "20px 20px 20px 20px",
+              borderColor: "white",
+              borderWidth: "3px",
+              color: "black",
+              borderStyle: "solid"
+            }}>Search</button>
+          </div>
+        </header>
+
+        <h1 style={{
+          fontSize: "52px",
+          paddingTop: "50px",
+          fontFamily: "fantasy",
+          fontWeight: "normal"
+        }}>Your Weather Source</h1>
+
+        <div style={{ marginTop: "5px", paddingRight: "10px", paddingBottom: "25px" }}>
+          {showCityName && <h1 style={{
+            textTransform: "capitalize",
+            backgroundColor: "lightblue",
+            minWidth: "110%",
+            borderColor: "black",
+            borderWidth: "4px",
+            borderStyle: "solid",
+            borderRadius: 30,
+            fontSize: "40px"
+          }}>{city}</h1>}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          {showCityName && <h1 style={{ fontSize: "44px", fontWeight: "bold"}}>Hourly Forecast</h1>}
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        {showExtra && (<div style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          padding: 20,
+          borderRadius: 20,
+          maxWidth: "100%",
+          maxHeight: "100%",
+          alignItems: "center",
+          textAlign: "center",
+          marginBottom: 50,
+          marginTop: 50,
+          backgroundColor: "lightblue",
+          borderColor: "black",
+          borderWidth: "10px",
+          borderStyle: "solid"
+        }}>
+          <p style={{ fontSize: "34px", paddingRight: "10px" }}>{hourWeather?.dt ? new Date(hourWeather?.dt * 1000).toLocaleDateString(undefined, { month: 'short', day: 'numeric', year: 'numeric' }) : ""}</p>
+          {hourWeather?.weather[0].main && (
+            <div>
+              {hourWeather?.weather[0].main === "Clear" && <img src="/images/sun.png" width={200} alt="Sunny" />}
+              {hourWeather?.weather[0].main === "Rain" && <img src="/images/rain.png" width={200} alt="Rainy" />}
+              {hourWeather?.weather[0].main === "Clouds" && <img src="/images/cloud.png" width={200} alt="Cloudy" />}
+              {hourWeather?.weather[0].main === "Snow" && <img src="/images/snow.png" width={200} alt="Snowy" />}
+            </div>
+          )}
+          <div style={{ display: "flex", flexDirection: "column", paddingLeft: "30px" }}>
+            <h3 style={{ fontSize: "18px", }}>{hourWeather?.weather[0].main}</h3>
+            <h3 style={{ fontSize: "18px" }}>{hourWeather?.main.temp.toFixed(1)}°C</h3>
+            <h3 style={{ fontSize: "18px" }}>{hourWeather?.wind.speed}km/h</h3>
+          </div>
+        </div>)}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div>
+          <div>
+            {showCityName && <h1 style={{ paddingBottom: "40px", fontSize: "44px", fontWeight: "bold" }}>5 Day Forecast</h1>}
+          </div>
+          {weatherForecast?.list.slice(0, 5).map((item, index) => (
+            <div key={index} style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              padding: 20,
+              borderRadius: 30,
+              maxWidth: "100%",
+              maxHeight: "100%",
+              alignItems: "center",
+              textAlign: "center",
+              marginBottom: 20,
+              backgroundColor: "#706bff",
+              borderColor: "black",
+              borderWidth: "10px",
+              borderStyle: "solid"
+            }}>
+              <p style={{ fontSize: "34px", paddingRight: "10px" }}>{new Date(new Date().getTime() + (index + 1) * 24 * 60 * 60 * 1000).toLocaleDateString(undefined, { month: 'short', day: 'numeric', year: 'numeric' })}</p>
+              {item.weather[0].main && (
+                <div>
+                  {item.weather[0].main === "Clear" && <img src="/images/sun.png" width={200} alt="Sunny" />}
+                  {item.weather[0].main === "Rain" && <img src="/images/rain.png" width={200} alt="Rainy" />}
+                  {item.weather[0].main === "Clouds" && <img src="/images/cloud.png" width={200} alt="Cloudy" />}
+                  {item.weather[0].main === "Snow" && <img src="/images/snow.png" width={200} alt="Snowy" />}
+                </div>
+              )}
+              <div style={{ display: "flex", flexDirection: "column", paddingLeft: "30px" }}>
+                <h3 style={{ fontSize: "18px" }}>{item.weather[0].main}</h3>
+                <h3 style={{ fontSize: "18px", textTransform: "capitalize" }}>{item.weather[0].description}</h3>
+                <h3 style={{ fontSize: "18px" }}>{item.main.temp.toFixed(1)}°C</h3>
+                <h3 style={{ fontSize: "18px" }}>{item.wind.speed}km/h</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+        <footer style={{ position: "relative", bottom: "0" }}>
+          <h4>2024 Jesse Welk ©</h4>
+        </footer>
+      </main>
+    </div>
   );
 }
